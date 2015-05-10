0511062f30e61d9de1dbc40e58e04c15bc8cbb02
Willem Bouma <wrb@outlook.com>
2015-05-10 14:25:20 +0200

    EventLog target added.

---
+++ StormReplayUploader/Targets/EventLogTarget.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security;
+
+namespace StormReplayUploader.Targets
+{
+    public class EventLogTarget : IStormReplayTarget
+    {
+        private readonly string sourceName = "StormReplay Uploader";
+
+        private bool available;
+
+        public EventLogTarget()
+        {
+            try
+            {
+                if (!EventLog.SourceExists(sourceName))
+                {
+                    EventLog.CreateEventSource(sourceName, "Application");
+                }
+
+                available = true;
+            }
+            catch (SecurityException) 
+            {
+                available = false;
+            }
+        }
+
+        public void Notify(string path)
+        {
+            if (!available)
+            {
+                return;
+            }
+
+            var message = String.Format("File: {0}", path);
+            EventLog.WriteEntry(sourceName, message, EventLogEntryType.Information, 1);
+        }
+
+        public void Subscribe(IObservable<string> observable)
+        {
+            observable.Subscribe(Notify);
+        }
+    }
+}
