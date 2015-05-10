23e5e192a2ccab6d93f3c859bbffb76a2334ecfa
Willem Bouma <wrb@outlook.com>
2015-05-10 10:34:58 +0200

    Remove unused usings

--- StormReplayUploader/ReplayWatcher.cs
+++ StormReplayUploader/ReplayWatcher.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reactive;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using StormReplayUploader.Targets;
 
 namespace StormReplayUploader
