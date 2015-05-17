d2a44b255156eb1fa361362b68ea4cd054a8e51f
Willem Bouma <wrb@outlook.com>
2015-05-17 22:31:13 +0200

    First attempt at using time to filter out files that have already been uploaded.

--- StormReplayUploader.Targets/ConsoleTarget.cs
+++ StormReplayUploader.Targets/ConsoleTarget.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.IO;
+using System.Reactive.Linq;
 using StormReplayUploader.Config;
 
 namespace StormReplayUploader.Targets
 {
     public class ConsoleTarget : IStormReplayTarget
     {
+        private DateTime LastCommit { get { return UploaderSettings.Get(Name); } }
+
         public string Name
         {
             get { return "ConsoleTarget"; }
@@ -14,13 +17,15 @@ namespace StormReplayUploader.Targets
         public void Notify(FileInfo fileInfo)
         {
             Console.WriteLine(fileInfo.FullName);
+
+            UploaderSettings.Update(Name, fileInfo.CreationTimeUtc);
         }
 
         public void Subscribe(IObservable<FileInfo> observable)
         {
-            var dt = UploaderSettings.Get(Name);
-
-            observable.Subscribe(Notify);
+            observable
+                .Where(file => file.CreationTimeUtc > LastCommit)
+                .Subscribe(Notify);
         }
     }
 }
