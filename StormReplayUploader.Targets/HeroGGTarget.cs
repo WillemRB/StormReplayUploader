55dfa2ae06f3e7e8ce5d02f7862a8382814ce8ee
Willem Bouma <wrb@outlook.com>
2015-05-17 16:41:34 +0200

    Add Hero.GG upload target

---
+++ StormReplayUploader.Targets/HeroGGTarget.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+namespace StormReplayUploader.Targets
+{
+    public class HeroGGTarget : IStormReplayTarget
+    {
+        public string Name
+        {
+            get { return "HeroGG"; }
+        }
+
+        public async void Notify(FileInfo fileInfo)
+        {
+            using (var client = new HttpClient())
+            using (var stream = new StreamReader(fileInfo.FullName))
+            {
+                var content = new StreamContent(stream.BaseStream);
+
+                var response = await client.PostAsync("http://upload.hero.gg/ajax/upload-replay", content);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    // Success
+                }
+            }
+        }
+
+        public void Subscribe(IObservable<FileInfo> observable)
+        {
+            observable.Subscribe(Notify);
+        }
+    }
+}
