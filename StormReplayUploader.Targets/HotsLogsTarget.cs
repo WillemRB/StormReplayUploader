5d679a6e18899d2b82625b1832b66dc9a6f1d937
Willem Bouma <wrb@outlook.com>
2015-05-18 10:52:27 +0200

    First version of HotsLogs uploader.

---
+++ StormReplayUploader.Targets/HotsLogsTarget.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace StormReplayUploader.Targets
+{
+    public class HotsLogsTarget : IStormReplayTarget
+    {
+        private readonly string ACCESS_KEY_ID = "AKIAIESBHEUH4KAAG4UA";
+        private readonly string SECRET_ACCESS_KEY = "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";
+
+        private readonly AmazonS3Client client;
+
+        public string Name
+        {
+            get { return "HotsLogs"; }
+        }
+
+        public HotsLogsTarget()
+        {
+            client = new AmazonS3Client(ACCESS_KEY_ID, SECRET_ACCESS_KEY);
+        }
+
+        public void Notify(FileInfo fileInfo)
+        {
+            var request = new PutObjectRequest()
+            {
+                BucketName = "heroesreplays",
+                Key = new Guid().ToString("D"),
+                FilePath = fileInfo.FullName,
+            };
+
+            var response = client.PutObject(request);
+
+            if (response.HttpStatusCode == HttpStatusCode.OK)
+            {
+                // Success
+            }
+        }
+
+        public void Subscribe(IObservable<FileInfo> observable)
+        {
+            observable.Subscribe(Notify);
+        }
+    }
+}
