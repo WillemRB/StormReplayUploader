e7751ed0bd3407a210cca16e433ccdb6239d1dcf
Willem Bouma <wrb@outlook.com>
2015-05-23 15:48:36 +0200

    Install script sets replayDirectory. When running as local system there is no MyDocuments path.

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,2 +1,21 @@
-.\StormReplayUploader.exe install
-.\StormReplayUploader.exe start
+# Load configuration
+$config = [xml](Get-Content "app.config")
+ 
+# Update attribute with path if it is empty
+$node = $config.SelectSingleNode("//uploaderConfiguration")
+if ($node.replayDirectory -eq "")
+{
+	# Determine path to replay files
+	$myDocuments = [Environment]::GetFolderPath("MyDocuments")
+	$hotsFolder = Join-Path $myDocuments "Heroes of the Storm"
+	$replayFolder = Join-Path $hotsFolder "Accounts"
+
+    $node.replayDirectory = $replayFolder.ToString()
+}
+ 
+# Save the result
+$config.Save()
+
+# Install service and start it
+.\StormReplayUploader.exe install --localsystem --autostart
+.\StormReplayUploader.exe start 
